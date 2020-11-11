febc2c17652a1f38bf3a31ea91696cf7a3569782
Anne Paakinaho <annetp@uef.fi>
2020-11-11 08:48:13 +0200

    making exercise3:logistic regression

--- data/create_alc.R
+++ data/create_alc.R
@@ -80,4 +80,64 @@ colnames(pormath)
 #save the file to data folder.
 write.csv(pormath,"C:/Users/silve/OneDrive - University of Eastern Finland/IODS-project 2020/IODS-project/data/pormath.csv")
 
+#it might be that after this, I am doing something wrong
+
+#joined columns; these variables are the ones that we want to use in this exercise.
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+# create a new data frame with only the joined columns
+alc <- select(pormath, one_of(join_by))
+
+# columns that were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+# print out the columns not used for joining
+notjoined_columns
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'pormath' with the same original name
+  two_columns <- select(pormath, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column  vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+
+# glimpse at the new combined data
+glimpse(alc)
+
+library(ggplot2)
+
+alc <- mutate (alc, alc_use=(Dalc + Walc)/2)
+g1 <- ggplot(data = alc, aes(x = alc_use))
+g1 + geom_bar()
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+# initialize a plot of 'high_use'
+g2 <- ggplot(alc, aes(high_use))
+
+# draw a bar plot of high_use by sex
+g2 + facet_wrap("sex") + geom_bar()
+
+library(tidyr)
+glimpse(alc)
+#there is 370 rows and 35 columns, it should be right. Having 384 rows would erroneous as it was said to us.
+
+# use gather() to gather columns into key-value pairs and then glimpse() at the resulting data
+gather(alc) %>% glimpse
+# draw a bar plot of each variable
+gather(alc) %>% ggplot(aes(value)) + facet_wrap("key", scales = "free") + geom_bar()
+
+#save 'alc' to data folder.
+write.csv(alc,"C:/Users/silve/OneDrive - University of Eastern Finland/IODS-project 2020/IODS-project/data/alc.csv")
 
